1efc8b133f3b573241c5014ed14905445db91267
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-09-09 21:29:23 +0300

    Updates

---
+++ precache-manifest.1efb358dcae4d48de7e25ac3bb856c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b133f73fb850ae48beb9220a9eb5a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73311e9f9d56b35718a9",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73311e9f9d56b35718a9",
+    "url": "./static/js/main.ffa8ca94.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
